b71911d479578b93a5ea7407ad7aef8cfe1cda5e
EscapeRandom <94118095+EscapeRandom@users.noreply.github.com>
2022-01-08 19:35:18 +0100

    Update of languagePack.js
    I updated the file because of grammar and expression reasons. I tried to give it a more natural sound, in case a native speaker like me would read the lines.

--- de/languagePack.js
+++ de/languagePack.js
@@ -11,25 +11,25 @@ export const languagePack = {
         "community":["Community"],
         "faq":["FAQ"],
         "home":["Home"],
-        "loging":["Login/Regestrieren"],
+        "loging":["Login/Registrieren"],
         "language":["#"]
     },
 
     splash:{
         "text":[],
         "update":["Letzte Version: 1.6.7 | Zuletzt geupdatet: 17-July-2019"],
-        "preupdate":["Vor-Release Version: 1.6.8 | Erste Phase"],
+        "preupdate":["Vor-Releaseversion: 1.6.8 | Erste Phase"],
         "download":["Herunterladen!"],
         "github":["GitHub!"]
     },
 
     alert:{
         "announcment":["Ankündigung"],
-        "text":["Unser Spiel wurde nie auf den Play Store hochgeladen. Wenn du unser Spiel dort findest. Könnte es Viren enthalten!"],
+        "text":["Unser Spiel wurde nie auf den Play Store hochgeladen. Wenn du unser Spiel dort findest, könnte es Viren enthalten!"],
     },
     about:{
         "about":["Über"],
-        "text":["osu!droid"," ist ein Rhytmus Spiel für Android. Es war ein Spiel, das es viele Jahren schon von","osu!","Community entwickelt wurde.","Es wird nun von unserer Community und Freunden weiterentwickelt. Außerdem ist, das Spiel kostenlos spielbar.","Das Spiel/Programm und Webseite ist nicht mit ","osu!","oder","ppy","gebunden."],
+        "text":["osu!droid"," ist ein Rhytmusspiel für Android. Es war ein Spiel, das schon viele Jahre ","osu!","Community entwickelt wurde.","Es wird nun von unserer Community und Freunden weiterentwickelt. Außerdem ist das Spiel kostenlos spielbar.","Das Spiel/Programm und Webseite ist nicht mit ","osu!","oder","ppy","gebunden."],
         "text1":["Das Spiel/Programm und Webseite ist nicht mit ","osu!","oder","ppy","gebunden."]
     },
 
@@ -38,27 +38,27 @@ export const languagePack = {
         "rank":["#"],
         "playername":["Spielername"],
         "score":["Score"],
-        "accuracy":["Accuracy"],
+        "accuracy":["Genauigkeit"],
         "region":["Land"],
-        "text": ["Jede Beatmap ist in unseren Spiel geranglistet. In, den Leaderboard kannst du die Top 100 von der ganze Welt sehen."]
+        "text": ["Jede Beatmap ist in unserem Spiel geranglistet. In dem Leaderboard kannst du die Top 100 von der ganzen Welt sehen."]
     },
 
     community:{
-        "community":["Betrette unsere Community bei!"],
+        "community":["Trete unserer Community bei!"],
         "qq":["QQ Gruppe","GruppenID: 577344771"],
         "baidu":["Baidu Forum","Nur Chinesisch!"],
-        "discord":["Discord","Tritt, unseren Discord Server bei und hab spaß mit andern osu!droid Spielern oder Herausforderungen!"],
+        "discord":["Discord","Tritt, unserem Discord-Server bei und hab Spaß mit andern osu!droid Spielern oder Herausforderungen!"],
         "bilibili":["Bilibili Offizieler Account"]
     },
 
     faq:{
         "faq":["Fragen und Antworten"],
-        "gameQuestion":["Habe, das Spiel nie gespielt, Wie fange ich an?"],
-            "gameAnswer":["Wir bevorzugen dir diesen Artikel anzuschauen."],
-        "beatmapsQuestion":["Wie, Wo, kann ich Beatmaps Downloaden?"],
-            "beatmapsAnswer":["Du kannst auf der","osu! Offiziele Webseite","Beatmaps herunterladen. Du kannst auch ein anderen Beatmap Download Server. Wie,","Chimu,","Sayobot,","etc","benutzen","oder du benutzt, den", "BeatmapService","für Android."],
+        "gameQuestion":["Habe, das Spiel nie gespielt. Wie fange ich an?"],
+            "gameAnswer":["Wir raten dir, dir diesen Artikel anzuschauen."],
+        "beatmapsQuestion":["Wie und wo kann ich Beatmaps downloaden?"],
+            "beatmapsAnswer":["Du kannst auf der","osu! Offiziele Webseite","Beatmaps herunterladen. Du kannst auch einen anderen Beatmap-Downloadserver. Wie,","Chimu,","Sayobot,","etc","benutzen","oder du benutzt, den", "BeatmapService","für Android."],
         "deviceQuestion":["Was ist die Anforderung, des Spiels?"],
-            "deviceAnswer":["Du kannst jeden Android Gerät benutzen. Die Mindest Android Version ist 2.3 oder höher.","Es ist bekannt, das bei einige Samsung Smartphones Kompatibilitäts Probleme gibt. Daher versuch mal eine alternatives Android-Betriebssystem."],
+            "deviceAnswer":["Du kannst jedes Android-Gerät benutzen. Die Mindestandroidversion ist 2.3 oder höher.","Es ist bekannt, das bei einigen Samsung-Smartphones Kompatibilitätsprobleme bestehen. Daher versuche eventuell ein alternatives Android-Betriebssystem."],
         "moreFaq":["Klick","Hier","Für weitere Fragen"]
     },
 
@@ -73,6 +73,6 @@ export const languagePack = {
         "email":["report@dgsrz.com | webmaster@acivev.com"],
         "others":["Anderes"],
         "email2":["webmaster@dgsrz.com | webmaster@acivev.com"],
-        "copyright":["Copyright © 2015-2020, osu!droid Team ♥ und Seiten Design von Geticer"]
+        "copyright":["Copyright © 2015-2020, osu!droid Team ♥ und Seitendesign von Geticer"]
     }
 } 
